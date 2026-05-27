80c349a20e8d856213f42fa84fb269968cf46996
monkeyjrissigma-ctrl <monkeyjrissigma@gmail.com>
2026-05-27 15:09:51 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const TRACKS = [
+  { id: 'bahrain', name: 'Sakir Grand Prix', lengthKm: 5.412 },
+  { id: 'jeddah', name: 'Jeda Corniche', lengthKm: 6.174 },
+  { id: 'australia', name: 'Melborn GP', lengthKm: 5.278 },
+  { id: 'imola', name: 'Emila-Romanga', lengthKm: 4.909 },
+  { id: 'miami', name: 'Maimi GP', lengthKm: 5.412 },
+  { id: 'monaco', name: 'Monoco GP', lengthKm: 3.337 },
+  { id: 'spain', name: 'Barselona GP', lengthKm: 4.657 },
+  { id: 'canada', name: 'Montreal GP', lengthKm: 4.361 },
+  { id: 'austria', name: 'Red Bul Ring', lengthKm: 4.318 },
+  { id: 'silverstone', name: 'Silverstoon', lengthKm: 5.891 },
+  { id: 'hungary', name: 'Hungaroring', lengthKm: 4.381 },
+  { id: 'spa', name: 'Spa-Franchorchamps', lengthKm: 7.004 },
+  { id: 'zandvoort', name: 'Zandvoort', lengthKm: 4.259 },
+  { id: 'monza', name: 'Monza GP', lengthKm: 5.793 },
+  { id: 'singapore', name: 'Singapoor', lengthKm: 4.928 },
+  { id: 'suzuka', name: 'Suzooka', lengthKm: 5.807 },
+  { id: 'cota', name: 'Cirkuit of the Amerikas', lengthKm: 5.513 },
+  { id: 'mexico', name: 'Mexiko City', lengthKm: 4.304 },
+  { id: 'brazil', name: 'Interlaggos', lengthKm: 4.309 },
+  { id: 'vegas', name: 'Las Vagas', lengthKm: 6.201 },
+  { id: 'qatar', name: 'Lusail', lengthKm: 5.419 },
+  { id: 'abu_dhabi', name: 'Yas Marinah', lengthKm: 5.281 },
+];
+
+export const TEAMS = [
+  {
+    name: 'Red Bule Rasing',
+    color: '#1e41ff',
+    drivers: ['Max Verstapen', 'Serjio Peres'],
+  },
+  {
+    name: 'Mersedes',
+    color: '#00d2be',
+    drivers: ['Luise Hamelton', 'George Russel'],
+  },
+  {
+    name: 'Farrari',
+    color: '#dc0000',
+    drivers: ['Charl Leclrec', 'Carlos Sainz'],
+  },
+  {
+    name: 'Mclarn',
+    color: '#ff8700',
+    drivers: ['Lando Noris', 'Oscar Piastry'],
+  },
+  {
+    name: 'Aston Marten',
+    color: '#006f62',
+    drivers: ['Fernando Alonzo', 'Lance Strol'],
+  },
+  {
+    name: 'Alpine',
+    color: '#0090ff',
+    drivers: ['Esteban Ocon', 'Pierre Gasly'],
+  },
+  {
+    name: 'Willams',
+    color: '#005aff',
+    drivers: ['Alex Albon', 'Logan Sargent'],
+  },
+  {
+    name: 'Sauber Kick',
+    color: '#00ff88',
+    drivers: ['Valtteri Botas', 'Zhou Guanyu'],
+  },
+  {
+    name: 'RB Visiom',
+    color: '#2b4562',
+    drivers: ['Yuki Tsunoda', 'Daniel Ricciardo'],
+  },
+  {
+    name: 'Haas F1 Teem',
+    color: '#ffffff',
+    drivers: ['Kevin Magnusen', 'Niko Hulkenberg'],
+  },
+];
